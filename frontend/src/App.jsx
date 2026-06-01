f9eddfe64936b2b3f1ffdc4b7a7e5d013e671aa1
Jilton Brian C <iamcampos657@gmail.com>
2026-06-01 21:54:30 +0300

    feat (portal): initialize react app with tailwind and base layout

---
+++ frontend/src/App.jsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <div className="min-h-screen bg-black-100 flex items-center justify-center">
+      <h1 className="text-4xl font-bold text-blue-600 border-4 border-blue-600 p-6 rounded-lg">
+        Tailwind is Working!
+      </h1>
+    </div>
+  )
+}
+
+export default App
